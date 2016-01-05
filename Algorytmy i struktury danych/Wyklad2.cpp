065f9d0638145d3db4893a34c6006ce271467d27
Pan Otlet <otlet0@gmail.com>
2016-01-05 06:49:25 +0100

    Fix it

--- Algorytmy i struktury danych/Wyklad2.cpp
+++ Algorytmy i struktury danych/Wyklad2.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
+
+/*
+ *  Program zostaÅ‚ napisany za pomocÄ… Å›rodowiska Code::Blocks 13.12 z kompilatorem
+ *
+ */
 
 using namespace std;
 
@@ -16,15 +22,18 @@ int walidacjad(){
 }
 
 void zadanie1() {
-	cout << "Podaj liczbê: ";
+	cout << "Podaj liczbÃª: ";
 	int x=walidacja();
 	if (x > 0) {cout << "Dodatnia";}
 	if (x < 0) {cout << "Ujemna";}
 	if (x == 0) {cout << "ZERO";}
 	string z="y";
-	while (z!="T" && z != "t" && z != "N" && z != "n") {cout << "\n\nChcesz kontynuowaæ dzia³anie zadania? [T/N] ";cin >> z;}
+	while (z!="T" && z != "t" && z != "N" && z != "n") {
+            cout << "\n\nChcesz kontynuowaÄ‡ dziaÅ‚anie zadania? [T/N] ";
+            cin >> z;
+    }
 	if (z == "T" || z == "t") {zadanie1();}
-	if (z == "N" || z == "n") {cout << "Dziêki za skorzystanie z tego programu";}
+	if (z == "N" || z == "n") {cout << "DziÃªki za skorzystanie z tego programu";}
 }
 
 void zadanie2() {
@@ -36,6 +45,7 @@ void zadanie2() {
 	if (x2<x3 && x3<x1){cout<<x2<<" "<<x3<<" "<<x1;}
 	if (x3<x1 && x1<x2){cout<<x3<<" "<<x1<<" "<<x2;}
 	if (x3<x2 && x2<x1){cout<<x3<<" "<<x2<<" "<<x1;}
+	cout<<endl;
 	system("pause"); system("cls");
 }
 
@@ -48,12 +58,12 @@ void zadanie3() {
 		case 2:	cout<<"Dwa";		break;
 		case 3:	cout<<"Trzy";		break;
 		case 4:	cout<<"Cztery";		break;
-		case 5:	cout<<"Piêæ";		break;
-		case 6:	cout<<"Szeœæ";		break;
+		case 5:	cout<<"PiÄ™Ä‡";		break;
+		case 6:	cout<<"SzeÅ›Ä‡";		break;
 		case 7:	cout<<"Siedem";		break;
 		case 8:	cout<<"Osiem";		break;
-		case 9:	cout<<"Dziewiêæ";	break;
-		case 10:cout<<"Dziesiêæ";	break;
+		case 9:	cout<<"DziewiÄ‡";	break;
+		case 10:cout<<"DziesiÄ™Ä‡";	break;
 		default:cout<<"ERROR 404";
 	}
 	system("pause"); system("cls");
@@ -75,19 +85,19 @@ void zadanie4() {
 void zadanie5() {
 	cout << "Podaj liczbe: ";
 	double x=walidacjad();
-	cout << "Podaj potêgê: ";
+	cout << "Podaj potÃªgÃª: ";
 	double n=walidacjad();
 	double wynik1=x;
 	system("cls");
 	for (double i=n;i>1;i--){wynik1*=x;}
-	cout<<"Wynik pêtln¹: "<<wynik1<<endl;
+	cout<<"Wynik pÃªtlnÂ¹: "<<wynik1<<endl;
 	cout<<"Wynik funkcja: "<<pow(x,n)<<endl;
 	system("pause");system("cls");
 }
 
 void zadanie6() {
 	int x=0;
-	while(x<10 || x>99){cout<<"Podaj liczbê 2-cyfrow¹: ";cin>>x;}
+	while(x<10 || x>99){cout<<"Podaj liczbÃª 2-cyfrowÂ¹: ";cin>>x;}
 	for (int i=10;i<=x;i++){cout<<i<<" ";}
 	cout<<endl;
 	system("pause");system("cls");
@@ -99,12 +109,12 @@ void zadanie7a() {
 	cout<<"Podaj 2 liczbe:";
 	int x2=walidacja();
     while(x1!=x2){if(x1>x2){x1-=x2;} else {x2-=x1;}}
-	cout<<"NWD: "<<x1<<endl;	
+	cout<<"NWD: "<<x1<<endl;
 	system("pause");
 }
 
 void zadanie7b() {
-	cout<<"Podaj iloœæ liczb z ci¹gu Fibonacci'ego";
+	cout<<"Podaj iloÅ›Ä‡ liczb z ciÄ…gu Fibonacci'ego";
 	int x=walidacja();
 	unsigned long long x1 = 0, x2 = 1;
 	for(int i=0;i<x;i++){cout<<x2<<" ";x2 += x1;x1 = x2-x1;}
@@ -113,13 +123,22 @@ void zadanie7b() {
 
 void menu() {
 	int wybor;
-	cout << "Podaj numer zadania: ";
+    cout<<"1. Sprawdza liczbÄ™ dodatnia lub ujemna lub zero"<<endl;
+    cout<<"2. UkÅ‚ada w porzÄ…dku rosnÄ…cym"<<endl;
+    cout<<"3. WyÅ“wietlamy slownie liczbe"<<endl;
+    cout<<"4. Czy liczba jest parzysta, czy nie parzysta"<<endl;
+    cout<<"5. Obliczanie potegi n-tej"<<endl;
+    cout<<"6. Wypisanie liczb od 10 do podanej od usera"<<endl;
+    cout<<"7. Algorytm Euklidesa"<<endl;
+    cout<<"8. Ciag Fibonaciego"<<endl;
+	cout<<"================================================"<<endl;
+	cout<<"Podaj numer zadania: ";
 	if (cin >> wybor) {
 		system("cls");
 		switch (wybor) {
-		case 1:	cout<<"Sprawdza liczbê dodatnia lub ujemna lub zero"<<endl;	zadanie1();		break;
-		case 2:	cout<<"Uk³ada w porz¹dku rosnacym"<<endl;					zadanie2();		break;
-		case 3:	cout<<"Wyœwietlamy slownie liczbe"<<endl;					zadanie3();		break;
+		case 1:	cout<<"Sprawdza liczbÃª dodatnia lub ujemna lub zero"<<endl;	zadanie1();		break;
+		case 2:	cout<<"UkÂ³ada w porzÂ¹dku rosnacym"<<endl;					zadanie2();		break;
+		case 3:	cout<<"WyÅ“wietlamy slownie liczbe"<<endl;					zadanie3();		break;
 		case 4:	cout<<"Czy liczba jest parzysta, czy nie parzysta"<<endl;	zadanie4();		break;
 		case 5:	cout<<"Obliczanie potegi n-tej"<<endl;						zadanie5();		break;
 		case 6:	cout<<"Wypisanie liczb od 10 do podanej od usera"<<endl;	zadanie6();		break;
